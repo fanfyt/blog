002aa905467776339b26738f43dd90a16395ee5a
fl <67448040+fanfyt@users.noreply.github.com>
2023-08-27 12:48:20 +0800

    Delete back directory

--- back/src/main/java/com/daydayup/back/service/read/UserReadService.java
+++
@@ -1,9 +0,0 @@
-package com.daydayup.back.service.read;
-
-import com.daydayup.back.entity.User;
-
-public interface UserReadService {
-    User getUserById(Integer id);
-
-    User getUserByName(String user);
-}
