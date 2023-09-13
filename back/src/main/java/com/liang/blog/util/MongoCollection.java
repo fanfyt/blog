48b89a633cafb1b1ed6612d102f3c70b5f6eed77
liang <fanfyt@163.com>
2023-09-13 23:39:08 +0800

    udpate

---
+++ back/src/main/java/com/liang/blog/util/MongoCollection.java
@@ -0,0 +1,15 @@
+package com.liang.blog.util;
+
+/**
+ * mongodb 集合
+ *
+ * @author liang
+ */
+
+public class MongoCollection {
+
+    public static final String ARTICLE = "article";
+    public static final String COMMENT = "comment";
+
+
+}
